d2d748bb816c60428760e50c5f11fb9d4a99d979
Vrukshit Jivani <vjivani2233@gmail.com>
2026-01-11 20:57:52 +0530

    Add files via upload

---
+++ 2_list_maxmin.py
@@ -0,0 +1,3 @@
+list1=[10,85,74,77,95,68,554,85]
+print('Maximum is ',max(list1))
+print('Minimum is ',min(list1))
---
+++ 3_tuple_len.py
@@ -0,0 +1,2 @@
+tuple1={'Jignesh',101,'Bca','Rajpra no2'}
+print("The lenght of tuple is ",len(tuple1))
